e7a154f06e62e284d03108d0890fae74056aa47e
zyhou <richard.maxime9@gmail.com>
2018-06-05 18:20:27 +0200

    front end call server

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Fetch } from 'react-request';
 
-const title = 'My Minimal React Webpack Babel Setup';
+class HelloWorld extends Component {
+    render() {
+        return (
+            <Fetch
+                url="http://localhost:3000/"
+                responseType="text"
+                transformData={helloWorld => {
+                    return {
+                        helloWorld,
+                    };
+                }}
+            >
+                {({ fetching, failed, data }) => {
+                    if (fetching) {
+                        return <div>Loading data...</div>;
+                    }
 
-ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
+                    if (failed) {
+                        return <div>The request did not succeed.</div>;
+                    }
+
+                    if (data) {
+                        return <div>{data.helloWorld}</div>;
+                    }
+
+                    return null;
+                }}
+            </Fetch>
+        );
+    }
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+});
+
+app.get('/', (req, res) => res.send('Hello World from server!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
