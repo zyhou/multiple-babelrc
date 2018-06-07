26243c9ff42ddc612832ba5cc61580eeda63c225
zyhou <richard.maxime9@gmail.com>
2018-06-07 09:29:42 +0200

    multiple babelrc

--- src/server/index.js
+++ src/server/index.js
@@ -11,6 +11,13 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/', (req, res) => res.send('Hello World from server!'));
+app.get('/', (req, res) => {
+    try {
+        throw 0;
+    } catch {
+        res.send('Hello World from server!');
+        console.log('Yay, code executes!');
+    }
+});
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
